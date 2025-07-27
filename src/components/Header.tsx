912dc9d56df49e89b887bc308c74fe247ccedabc
Sahas-Pathirana <sahaspathirana2006@gmail.com>
2025-07-27 22:21:57 +0530

    changed header text color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,11 +43,13 @@ const Header = () => {
               <Link
                 key={item.name}
                 to={item.path}
+               
                 className={`relative px-4 py-2 font-medium transition-all duration-300 ${
                   isActive(item.path)
-                    ? 'text-blue-600'
-                    : 'text-gray-700 hover:text-blue-600'
+                    ? 'text-gray-50'
+                    : 'text-white hover:bg-gray-50 hover:rounded-lg hover:text-blue-600'
                 }`}
+
               >
                 {item.name}
                 {isActive(item.path) && (
@@ -78,7 +80,7 @@ const Header = () => {
                   className={`px-4 py-3 rounded-lg font-medium transition-all duration-300 ${
                     isActive(item.path)
                       ? 'bg-blue-50 text-blue-600 border-l-4 border-blue-600'
-                      : 'text-gray-700 hover:bg-gray-50 hover:text-blue-600'
+                      : 'text-white hover:bg-gray-50 hover:text-blue-600'
                   }`}
                 >
                   {item.name}
